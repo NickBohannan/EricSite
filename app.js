2c817c01472c89d1c91236b67d18173b2fd1d4f2
Nick Bohannan <nbohannan@gmail.com>
2018-08-11 23:20:33 -0500

    worked on gallery page

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+
 app.use(morgan("dev"));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("/index.html");
+});
+
+app.get("*", (req, res) => {
+  res.send("Ah, ah, ah, you didn't say the magic word.");
+});
+
+app.listen(4500, () => console.log("Server listening on port 4500"));
 
 module.exports = app;
